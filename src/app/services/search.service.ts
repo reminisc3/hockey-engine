5f1de9a18d0d902103b44f7324da489b9d9aa6d2
Kyle Ketterer <reminisc3@gmail.com>
2022-01-03 17:34:57 -0500

    SearchService updates

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, of } from 'rxjs';
+import { DatabaseService } from './database.service'
+
+export interface SearchResult {
+  title: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  searchResults$ = new Subject<SearchResult[]>();
+  private searchResults: SearchResult[] = [];
+
+  constructor(private dbService: DatabaseService) { }
+
+  search(query: string) {
+
+    this.searchResults = [];
+
+    //TOO: Search DB here
+
+    this.searchResults$.next(this.searchResults);
+
+  }
+
 }
